c556cb9fc03e42ba9b49271e48350d437c99742a
raineryue <jinlei_yue@sina.com>
2015-12-04 14:58:01 +0800

    Initial Commit

---
+++ 20151204002-UITableView-LOLPlain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20151204002-UITableView-LOLPlain
+//
+//  Created by Rainer on 15/12/4.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20151204002-UITableView-LOLPlain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20151204002-UITableView-LOLPlain
+//
+//  Created by Rainer on 15/12/4.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
